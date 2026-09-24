3bb83fe04531881b0e17375cdcbce7cc4dfefc6d
Dhruv Nayak <dhruvr.nayak@gmail.com>
2026-09-24 15:53:19 +0530

    refactor: replace old PolarOps frontend with Insight UI

--- frontend/src/hooks/useScenarioSimulation.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from "@tanstack/react-query";
-import { simulateScenario, type ScenarioSimulateRequest, type ScenarioSimulateResponse } from "../lib/api";
-
-export function useScenarioSimulation() {
-  return useMutation<ScenarioSimulateResponse, Error, ScenarioSimulateRequest>({
-    mutationFn: (request: ScenarioSimulateRequest) => simulateScenario(request),
-  });
-}
